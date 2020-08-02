38de049b0dfd92854b1e25dd0e584949e3446c24
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2020-08-02 20:44:49 +0430

    Fixed Some Bugs; Added GD extension to to php Laravel Configured and some extra features deleted

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Gregwar\Image\Image;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function (Request $request) {
+    return ['yes'];
 });
