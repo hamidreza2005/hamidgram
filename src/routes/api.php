138b3a8a23e4849989484591f6980e5a9d0bb826
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2020-08-04 17:06:18 +0430

    Added Limit to reset Password Route

--- src/routes/api.php
+++ src/routes/api.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (Request $request) {
 //    \Illuminate\Support\Facades\Cache::put('user',\App\User::find(1));
-    return new \App\Mail\VerificationEmail(\App\User::find(1));
+    return ['ok'];
 });
 
 Route::post('/login','AuthController@login')->name('login');
 Route::post('/register','AuthController@register')->name('register');
 Route::get('/confirmation/emailConfirmation/{code}','AuthController@emailConfirm')->name('emailConfirmation');
-Route::post('/password/reset','AuthController@resetPassword')->name('resetPassword');
+Route::post('/password/reset','AuthController@resetPassword')->name('resetPassword')->middleware('throttle:1,10');
 
 
